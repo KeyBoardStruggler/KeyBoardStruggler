6f4180d53c73bc80e09783f806eefb0d58f756b8
Win10 <Win10@UKIDENT>
2022-11-25 15:40:36 +0100

    code: .. than that interface

--- FakeNotMoreThanThat/Contracts/IThanThat.cs
+++ FakeNotMoreThanThat/Contracts/IThanThat.cs
@@ -4,7 +4,9 @@ using System.Text;
 
 namespace FakeNotMoreThanThat.Contracts
 {
-    interface IThanThat
+    public interface IThanThat
     {
+        bool ThakeThat { get; }
+        int PickIt(int countOfPickups);
     }
 }
